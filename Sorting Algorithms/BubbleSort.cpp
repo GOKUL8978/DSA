48e788390d7b0d43e092accb700a088d7f2d6f84
GOKUL U <111762211+GOKUL8978@users.noreply.github.com>
2024-06-07 12:05:54 +0530

    Create BubbleSort.cpp

---
+++ Sorting Algorithms/BubbleSort.cpp
@@ -0,0 +1,27 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int arr[]={1,5,2,9,3,10};
+    int i,j;
+    bool swapped;
+    int n = sizeof(arr)/sizeof(arr[0]);
+    for(int i=0;i<n-1;i++){
+        swapped = false;
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+                swapped=true;
+            }
+        }
+        if(swapped==false){
+            break;
+        }
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<< " ";
+    }
+}
